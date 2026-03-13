e185178ff4a62a42e399d226c97348d3c232eaed
Succex7 <successoghenegare7@gmail.com>
2026-03-13 14:08:59 +0100

    correct upload UI, prgress bar and uploadind file size

--- js/upload.js
+++ js/upload.js
@@ -5,10 +5,6 @@ const dropZone = document.getElementById("dropZone");
 
 uploadBtn.addEventListener("click", () => fileInput.click());
 
-dragLink.addEventListener("click", () => {
-  fileInput.click();
-});
-
 fileInput.addEventListener("change", handleFiles);
 
 dropZone.addEventListener("dragover", (e) =>{
@@ -35,32 +31,66 @@ createUploadItem(file);
 }
 
 
+function formatSize(bytes){
+
+  const mb = bytes / (1024 * 1024);
+
+  if(mb < 1){
+  return (bytes / 1024).toFixed(1) + " KB";
+}
+
+return mb.toFixed(2) + " MB";
+
+}
+
 function createUploadItem(file){
 
 const item = document.createElement("div");
 item.className = "upload-item";
 
 item.innerHTML = `
-<div class="file-info">
-<span>${file.name}</span>
-<span class="status">Uploading...</span>
+<div class="upload-top">
+
+<div class="file-left">
+<i class="fa-solid fa-file file-icon"></i>
+
+<div class="file-text">
+<strong>${file.name}</strong>
+<small class="file-size">0 MB / ${formatSize(file.size)}</small>
+</div>
+
+</div>
+
+<div class="file-action">
+<i class="fa-solid fa-xmark cancel"></i>
+</div>
+
 </div>
 
 <div class="progress-bar">
 <div class="progress"></div>
+</div>
+
+<div class="upload-status">
+<span class="status">Uploading...</span>
+<span class="percent">0%</span>
 </div>
 `;
 
 uploadList.appendChild(item);
 
 const progress = item.querySelector(".progress");
+const percentText = item.querySelector(".percent");
+const sizeText = item.querySelector(".file-size");
 const status = item.querySelector(".status");
+const cancelBtn = item.querySelector(".cancel");
+const action = item.querySelector(".file-action");
 
 let percent = 0;
 
 const interval = setInterval(()=>{
 
-percent += Math.random()*15;
+percent += Math.random()*12;
 
 if(percent >= 100){
 
@@ -68,12 +98,39 @@ percent = 100;
 clearInterval(interval);
 
 item.classList.add("completed");
+
 status.textContent = "Completed";
+
+item.querySelector(".progress-bar").style.display = "none";
+
+percentText.style.display = "none";
+
+action.innerHTML = `<i class="fa-solid fa-trash delete"></i>`;
+
+const deleteBtn = action.querySelector(".delete");
+
+deleteBtn.addEventListener("click",()=>{
+item.remove();
+});
 
 }
 
 progress.style.width = percent + "%";
 
+percentText.textContent = Math.floor(percent) + "%";
+
+const uploaded = (file.size * percent) / 100;
+
+sizeText.textContent = `${formatSize(uploaded)} / ${formatSize(file.size)}`;
+
 },400);
+
+cancelBtn.addEventListener("click",()=>{
+
+clearInterval(interval);
+
+item.remove();
+
+});
 
 }
